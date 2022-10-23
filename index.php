4fe2129663146addb73833dcc8013db6c28d20af
Vrezh Fedora <vrezhraiden@gmail.com>
2022-10-23 20:17:12 +0200

    image centering

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <div class = "frame">
+        <img class = "image" src = "/assets/images/NicePng_instagram-circle-logo-png_475136.png"> 
+    </div>
+</body>
+</html>
