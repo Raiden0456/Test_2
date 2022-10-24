49f7b62ba0663be1e506cf89303aa11ce26f2e88
Vrezh Fedora <vrezhraiden@gmail.com>
2022-10-24 18:02:10 +0200

    unique insert

--- DB.php
+++ DB.php
@@ -21,18 +21,29 @@
     function upload_csv($filename)
     {
         global $connect;
+        //открываем файл для чтения//
         $file = fopen($filename, "r");
+        //рекурсивно перебиравем каждую строку файла и вносим в бд только новые записи с уникальным id//
         while (($getData = fgetcsv($file, 5000, ",")) !== FALSE)
         {
-            $sql = "INSERT into users (id,phone,email) 
-                   values ('".$getData[0]."','".$getData[1]."','".$getData[2]."')";
+            $sql = "
+            INSERT  `users` (`id`, `phone`, `email`) 
+                SELECT  '".$getData[0]."','".$getData[1]."', '".$getData[2]."'
+            WHERE   NOT EXISTS 
+                    (   SELECT  `id`
+                        FROM    `users` 
+                        WHERE   `id` = '".$getData[0]."'
+                    );
+            ";
             $result = $connect->query($sql);
         }
+        //обработчик ошибок//
         if(!isset($result))
             return error_log("Invalid File:Please Upload CSV File.");
         else {
             return error_log("CSV File has been successfully Imported.");
         }
+        //закрываем файл//
         fclose($file);    
     }
 
